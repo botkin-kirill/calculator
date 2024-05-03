40d346bde93026d459beb9ac42ee63d7beb1b618
veiiev <worldloflwarcraft@mail.ru>
2024-05-03 18:25:11 +0400

    добавил ввод значений из консоли

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("введите выражение, разделяйте переменные пробелом");
-        Calculator.calculate("I - X");
+        String input = new Scanner(System.in).nextLine();
+        Calculator.calculate(input);
     }
 }
